2100cff701402db3c56ff86b36a00d56ae8fec53
Arpit21345 <arpit21345j@gmail.com>
2024-08-01 01:37:17 +0530

    Add item page

--- admin/vite-project/src/components/Sidebar/Sidebar.jsx
+++ admin/vite-project/src/components/Sidebar/Sidebar.jsx
@@ -12,12 +12,12 @@ const Sidebar = () => {
           <p>Add Items</p>
         </NavLink>
         <NavLink to='./list' className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
-          <p>Add Items</p>
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
         </NavLink>
         <NavLink to='./orders' className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
-          <p>Add Items</p>
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
         </NavLink>
       </div>
     </div>
